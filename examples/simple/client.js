df04e2c8b04fd6b98ec0087e6b231f059d1b7b56
yojimbo87 <bosak.tomas@gmail.com>
2011-09-07 23:17:13 +0200

    Testing client side feedr stuff.

--- examples/simple/client.js
+++ examples/simple/client.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    var source = new EventSource("/events");
+    /*var source = new EventSource("/events");
     
     source.onmessage = function(e) {
         $("#debug").append(
@@ -15,16 +15,17 @@ $(document).ready(function() {
             (new Date()).toLocaleTimeString() + " " +
             JSON.stringify(e) + "<br />"
         );
-    };
+    };*/
+    
+    feedr.on("connect", function(data) {
+        $("#debug").append(
+            "EVENT " + 
+            (new Date()).toLocaleTimeString() + " " +
+            data + "<br />"
+        );
+    });
+    
+    feedr.connect({});
     
     //$('#debug').load("http://rt01.developmententity.sk:8080/text");
 });
-
-/*var feedr = (function(undefined) {
-    
-    
-    
-    return {
-		
-	}
-}());*/
